c477ea79e2105cc4616c2dcae41486359d836f33
rmslrk <rmslrk@gmail.com>
2022-01-26 15:37:47 +0900

    listMapper param 수정

--- src/main/java/repository/ListMapper.java
+++ src/main/java/repository/ListMapper.java
@@ -1,19 +1,20 @@
 package repository;
 
 import domain.ListDTO;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ListMapper {
-    void createList(ListDTO listDTO);
+    void createList(@Param("list") ListDTO listDTO);
 
     ListDTO getList(Long id);
 
-    void updateList(ListDTO listDTO);
+    void updateList(@Param("list") ListDTO listDTO);
 
-    void moveList(ListDTO listDTO);
+    void moveList(@Param("list") ListDTO listDTO);
 
-    void deleteList(ListDTO listDTO);
+    void deleteList(@Param("list") ListDTO listDTO);
 
     Long isExistsList(Long id);
 }
